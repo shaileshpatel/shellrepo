041ea1fac2c98f0996b034d31192890c541f65c3
ShaileshPatel <gia_mcf@yahoo.com>
2012-01-30 23:20:28 -0500

    shell code to validate YYYY MM DD date format

---
+++ datevalid.sh
@@ -0,0 +1,60 @@
+usage="usage - ksh datevalid.sh YYYY MM DD"
+
+lastday()  {
+        integer year month leap
+        set -A mdays sp 31 28 31 30 31 30 31 31 30 31 30 31
+
+        year=$1
+        if ((${#year} != 4)) ; then
+                return 1
+        fi
+        month=$2
+        if ((month<1 || month> 12)) ; then
+                return 1
+        fi
+
+        if ((month != 2)) ; then
+                return 0
+        fi
+
+        leap=0
+        if ((!(year%100))); then
+                ((!(year%400))) && leap=1
+        else
+                ((!(year%4))) && leap=1
+        fi
+
+        feblength=28
+        ((leap)) && feblength=29
+        print $feblength
+        return 0
+}
+
+    if (($# == 3)) ; then
+                year=$1;month=$2;day=$3
+                echo $year | grep -Eq '[0-9]+$'
+                int_year=$?
+                if ((int_year != 0)); then
+                        return 1
+                fi
+                echo $month | grep -Eq '[0-9]+$'
+                int_month=$?
+                 if ((int_month != 0)); then
+                        return 1
+                fi
+                echo $day | grep -Eq '[0-9]+$'
+                int_day=$?
+                if ((int_day != 0)); then
+                        return 1
+                fi
+                
+                if ((int_year<=0 && int_day<=0 && int_month<=0)) ; then
+                        lastday $1 $2
+                        return $?
+                else 
+                 return 1
+                fi
+        else
+                print -u2 - "$usage"
+                exit 1
+        fi
